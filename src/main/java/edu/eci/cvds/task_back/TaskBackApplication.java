a20f034788060c716916c4f1a51fc5b75fbd6a62
JaiderVargas05 <Jaider.vargas-n@mail.escuelaing.edu.co>
2024-09-28 13:31:55 -0500

    Exclude classes from jacoco

--- src/main/java/edu/eci/cvds/task_back/TaskBackApplication.java
+++ src/main/java/edu/eci/cvds/task_back/TaskBackApplication.java
@@ -1,4 +1,4 @@
-package edu.eci.cvds.task_back.Services;
+package edu.eci.cvds.task_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
