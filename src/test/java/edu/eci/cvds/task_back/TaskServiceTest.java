1fd366855370d7457ac235da55a3115eebb4ab0a
JAGBytes <jorge.gamboa-s@mail.escuelaing.edu.co>
2024-09-29 16:01:03 -0500

    primeras pruebas a service

--- src/test/java/edu/eci/cvds/task_back/TaskServiceTest.java
+++ src/test/java/edu/eci/cvds/task_back/TaskServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -119,5 +120,135 @@ public class TaskServiceTest {
 
         // Verificar que el método deleteTask del repositorio fue llamado una vez con la tarea correspondiente
         verify(taskRepository, times(1)).deleteTask(task);
+    }
+
+    /*Pruebas Lab5*/
+
+    @Test
+    void SucessfullIDQuery() {
+        // Crear un objeto Task con ID "2" y otros detalles
+        Task task = new Task("2", "Test Task", "Description", LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        // Configurar el comportamiento del mock para devolver el task creado al buscar por ID "2"
+        when(taskRepository.findTaskById("2")).thenReturn(task);
+
+        // Llamar al método getTask en el servicio para obtener la tarea por ID
+        Task result = taskService.getTask("2");
+
+        // Verificar que el resultado no es nulo
+        assertNotNull(result);
+        // Comprobar que el ID de la tarea resultante es "2"
+        assertEquals("2", result.getId());
+    }
+    @Test
+    void UnsucessfullIDQuery() {
+
+
+        // Se intenta obtener una tarea que no existe con su ID
+        Task result = taskService.getTask("2");
+
+        // Llamar al método getTasks en el servicio para obtener todas las tareas
+        List<Task> results = taskService.getTasks();
+
+        // Verificar que el resultado es nulo
+        assertEquals(results.size(),0);
+
+        // Verificar que la tarea consultada con el ID no existe
+        assertNull(result);
+    }
+
+    @Test
+    void successfulTaskQuery() {
+        // Configurar el comportamiento del mock para devolver una lista vacía inicialmente
+        when(taskRepository.findAllTasks()).thenReturn(Collections.emptyList());
+
+        // Llamar al método getTasks en el servicio para obtener todas las tareas
+        List<Task> results = taskService.getTasks();
+
+        // Verificar que la lista de tareas está vacía
+        assertEquals(0, results.size());
+
+        // Crear un objeto Task con ID "2" y otros detalles
+        Task task = new Task("2", "Test Task", "Description", LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        // Guardar la tarea
+        taskService.saveTask(task);
+
+        // Configurar el comportamiento del mock para devolver la tarea creada al buscar por ID "2"
+        when(taskRepository.findTaskById("2")).thenReturn(task);
+
+        // Llamar al método getTask en el servicio para obtener la tarea por ID
+        Task result = taskService.getTask("2");
+
+        // Verificar que el resultado no es nulo
+        assertNotNull(result);
+
+        // Configurar el comportamiento del mock para devolver una lista con la tarea creada
+        when(taskRepository.findAllTasks()).thenReturn(Collections.singletonList(task));
+
+        // Llamar al método getTasks en el servicio para obtener todas las tareas
+        List<Task> results2 = taskService.getTasks();
+
+        // Verificar que la lista contiene una tarea
+        assertEquals(1, results2.size());
+    }
+
+    @Test
+    void successfulTaskDelete() {
+
+        // Crear un objeto Task con ID "2" y otros detalles
+        Task task = new Task("2", "Test Task", "Description", LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        // Guardar la tarea
+        taskService.saveTask(task);
+
+        when(taskRepository.findAllTasks()).thenReturn(Collections.singletonList(task));
+
+        // Llamar al método getTasks en el servicio para obtener todas las tareas
+        List<Task> results = taskService.getTasks();
+
+        // Verificar que la lista contiene una tarea
+        assertEquals(1, results.size());
+
+        taskService.deleteTask("2");
+
+        // Configurar el comportamiento del mock para devolver una lista con la tarea creada
+        when(taskRepository.findAllTasks()).thenReturn(Collections.emptyList());
+
+        List<Task> result2 = taskService.getTasks();
+
+        assertEquals(0,result2.size());
+    }
+
+    @Test
+    void successfulTaskDeleteAndGetNullwithID() {
+
+        // Crear un objeto Task con ID "2" y otros detalles
+        Task task = new Task("2", "Test Task", "Description", LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+
+        // Guardar la tarea
+        taskService.saveTask(task);
+
+        when(taskRepository.findAllTasks()).thenReturn(Collections.singletonList(task));
+
+        // Llamar al método getTasks en el servicio para obtener todas las tareas
+        List<Task> results = taskService.getTasks();
+
+        // Verificar que la lista contiene una tarea
+        assertEquals(1, results.size());
+
+        taskService.deleteTask("2");
+
+        // Configurar el comportamiento del mock para devolver una lista con la tarea creada
+        when(taskRepository.findAllTasks()).thenReturn(Collections.emptyList());
+
+        List<Task> result2 = taskService.getTasks();
+
+        assertEquals(0,result2.size());
+
+        when(taskRepository.findTaskById("2")).thenReturn(null);
+
+        Task taskFinal = taskService.getTask("2");
+        assertNull(taskFinal);
     }
 }
