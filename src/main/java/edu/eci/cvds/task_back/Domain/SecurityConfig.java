18b229a08b43d436b3850930caef8fc9b296cdd5
LaaSofiaa <laura.gil-c@mail.escuelaing.edu.co>
2024-10-08 17:22:33 -0500

    securityconfig

--- src/main/java/edu/eci/cvds/task_back/Domain/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/task_back/Domain/SecurityConfig.java
@@ -3,28 +3,34 @@ package edu.eci.cvds.task_back.Domain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import edu.eci.cvds.task_back.Services.UserService;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                    .antMatchers("/taskManager/**").authenticated()  // Protege todas las rutas relacionadas con tareas
-//                    .antMatchers("/auth/**").permitAll()  // Permite acceso libre a la autenticación
-//                .and()
-//                .formLogin()  // Habilita el inicio de sesión por formulario
-//                .and()
-//                .logout().permitAll();  // cerrar sesión
-//        return http.build();
-//    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-}
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth-> auth
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/auth/login")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
