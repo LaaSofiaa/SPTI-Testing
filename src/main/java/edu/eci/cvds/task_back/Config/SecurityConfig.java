43153ace514411813c4d9a07df42d9792fd1d81f
JaiderVargas05 <Jaider.vargas-n@mail.escuelaing.edu.co>
2024-10-08 20:32:04 -0500

    seguridad Terminada

--- src/main/java/edu/eci/cvds/task_back/Config/SecurityConfig.java
+++ src/main/java/edu/eci/cvds/task_back/Config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package edu.eci.cvds.task_back.Domain;
+package edu.eci.cvds.task_back.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
