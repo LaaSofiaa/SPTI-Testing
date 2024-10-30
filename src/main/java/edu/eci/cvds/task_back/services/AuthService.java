d1781d0012a95fa32b9a09eba13caaea615926c0
ccastano46 <castanocamilo522@gmail.com>
2024-10-30 07:25:10 -0500

    fixing Exceptions

--- src/main/java/edu/eci/cvds/task_back/services/AuthService.java
+++ src/main/java/edu/eci/cvds/task_back/services/AuthService.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-
+//Para cambio
 @Service
 
 public class AuthService {
